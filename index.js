4b005bf32dee0040f001208bc70e270e532e34af
ronald vilorio <soccer23415@gmail.com>
2018-05-14 19:44:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ function nameToAttributes(drivers){
   return drivers.map(function(driver){ return Object.assign({},{firstName: driver.split(" ")[0], lastName: driver.split(" ")[1]})})
 }
 function attributesToPhrase(drivers){
-  // debugger
+  
   return drivers.map(function(driver){ return `${driver["name"]} is from ${driver["hometown"]}`})
 }
